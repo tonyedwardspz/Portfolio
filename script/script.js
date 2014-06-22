b2431da784c4f254092d7990c00842bac71d0b10
Tony Edwards <tony.edwards@gmail.com>
2014-06-22 18:39:56 +0100

    0.1.3
    - Hover effects added to the social footer links via jQuery
    - bottom border style added

--- script/script.js
+++ script/script.js
@@ -8,34 +8,40 @@ $(document).ready(function(e){
         toggleMenu();
     });
     
-    // change the default psuedo classes for the main 
-    // navigation on smaller screens
-//    if ($(window).width() < breakpoint) {
-//        $('.menu').removeClass('force-darkgrey');
-//        $('.menu').addClass('force-white');
-//        
-//    } 
-//    if ($(window).width() >= breakpoint) {
-//        $('.menu').removeClass('force-white');
-//        $('.menu').addClass('force-darkgrey');
-//        
-//    }
-   
-    
-    
-    
-    
+     $('.social-links img').hover(function () {
+        
+        var elmHeight = $(this).height() - 4;
+        var elmWidth = $(this).width() - 4;
+        
+        $(this).css(
+            'width', elmHeight + 'px',
+            'height',elmWidth + 'px'
+        );
+         $(this).addClass('hover-margin');
+        
+    }, function() {
+        $(this).removeAttr('style');
+        $(this).removeClass('hover-margin');
+        
+    });
+       
 });
+
+
+
 
 $(window).resize(function() {
     var browserWidth = $(window).width();
     
-    // Remove any attached styles to prevent
-    // missing navigation on window resize.
     if (browserWidth <= breakpoint) {
         $('.menu').removeAttr('style');
     }
+    
+    toggleMenu();
+    changeMenuColor();
+
 });
+
 
 // Insert inline css into 'menu' to show / hide menu.
 function toggleMenu(){
@@ -45,5 +51,23 @@ function toggleMenu(){
         $('.menu').css('display', 'block');
     }else if (isDisplay == 'block'){
         $('.menu').css('display', 'none');
+    }
+    
+    changeMenuColor();
+}
+
+
+// change the default psuedo classes for the main navigation on smaller screens
+function changeMenuColor() {
+    
+    if ($(window).width() < breakpoint) {
+        $('.menu').removeClass('force-darkgrey');
+        $('.menu').addClass('force-white');
+        
+    } 
+    if ($(window).width() >= breakpoint) {
+        $('.menu').removeClass('force-white');
+        $('.menu').addClass('force-darkgrey');
+        $('.menu').css('display', 'block');
     }
 }
