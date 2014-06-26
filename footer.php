0ae9c348eade757c36fcc0832509f9b0175727c3
Tony Edwards <tony.edwards@gmail.com>
2014-06-26 23:31:09 +0100

    1.0 - Wordpress conversion
    Setting up the project for conversion to a Wordpress theme

---
+++ footer.php
@@ -0,0 +1,25 @@
+            </div>
+            
+        </div>   
+    </div>
+
+    <footer>
+        <div class="container">
+            
+            <span id="footer-text">Get in touch</span>
+
+            <ul class="social-links">
+                <li><a href="http://uk.linkedin.com/in/tonyedwardspz"><img src="images/social/linkedin-48x48.png" alt="linkedin" /></a></li>
+                <li><a href="http://www.dribbble.com/tonyedwardspz"><img src="images/social/dribbble-48x48.png" alt="Dribble" /></a></li>
+                <li><a href="http://www.twitter.com/tonyedwardspz"><img src="images/social/twitter-48x48.png" alt="Twitter" /></a></li>
+                <li><a href="http://plus.google.com/109731755368109204853"><img src="images/social/google+-48x48.png" alt="GitHub" /></a></li>
+                <li><a href="http://github.com/tonyedwardspz"><img src="images/social/github-48x48.png" alt="Google+" /></a></li>
+            </ul>
+            
+        </div>
+    </footer>
+
+    <?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title><?php wp_title(); ?> | <?php bloginfo('name'); ?></title>
+	
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    
+    <link rel="stylesheet" type="text/css" href="style/normalize.css" />
+    <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+    
+    <!--[if lt IE 9]>
+        <script src="script/html5shiv.js"></script>
+    <![endif]-->
+    
+    <script type="text/javascript" src="script/jQuery-1-1.js" /></script>
+    <script type="text/javascript" src="script/script.js" /></script>
+
+    <?php wp_head(); ?>
+
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="grid">
+            
+            <header class="unit full-width">  
+                
+                <?php $logo= get_option('portfolio_logo', IMAGES.'/tony_edwards.png'); ?>
+                <span><a href="index.html"><img src="<?php echo $logo; ?>" alt="<?php bloginfo('name'); ?>" /></a></span>
+                
+<!--
+                <nav>
+                    <ul id="toggle-menu">
+                        <li><a href="#" id="pull"><img src="images/nav-icon.png" /></a></li>
+                    </ul>
+                    
+                    <ul class="menu force-darkgrey">
+                        <li class="active"><a href="index.html">Home</a></li>
+                        <li><a href="about.html">About</a></li>
+                        <li><a href="portfolio.html">Portfolio</a></li>
+                        <li><a href="blog.html">Blog</a></li>
+                        <li><a href="contact.html">Contact</a></li>
+                    </ul>
+                </nav>
+-->
+                
+                <?php wp_nav_menu( array('menu' => 'Main', 'container' => 'nav' )); ?>
+
+            </header>
+            
+            <div class="content">
