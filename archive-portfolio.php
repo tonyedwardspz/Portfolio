172f11a4cf3c5c2137bd1855df279d5b8aa2d5a7
Tony Edwards <tony.edwards@gmail.com>
2014-07-01 22:20:24 +0100

    Dead end

---
+++ archive-portfolio.php
@@ -0,0 +1,31 @@
+<?php get_header(); ?>
+                
+            <div class="unit full-width content-block">
+
+                <h1>My portfolio</h1>
+
+                <p>Here you will find some of my Website design work, including projects for clients and University.</p>
+
+                <span class="breadcrumbs">All // Website // Games // PHP // Javascript</span>
+
+            </div>
+
+            <?php if (have_posts()) : ?>
+
+            <?php while (have_posts()) : the_post(); ?>
+
+            <div class="unit one-of-three portfolioItem">
+
+                <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('te_project'); ?></a>
+
+                <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+
+                <p><?php echo get_the_excerpt(); ?> <a href="<?php the_permalink(); ?>">read more...</a></p>
+
+            </div>
+
+            <?php endwhile; ?>
+
+            <?php endif; ?>
+
+<?php get_footer(); ?>
--- portfolio-post.php
+++ portfolio-post.php
@@ -1,5 +1,32 @@
-<?php
+<?php get_header(); ?>        
 
+            <div class="unit full-width">
+                
+                <?php if (have_posts()) : ?>
+					
+				<?php while (have_posts()) : the_post(); ?>
+				
+						<h1><?php the_title(); ?></h1>
 
+							<?php the_post_thumbnail('te_project'); ?>
+						
+							<?php the_content('Read the rest of this entry &raquo;'); ?>
 
-?>
+							<?php
+								if(function_exists('te_projects_get_link')){
+									$te_link= te_projects_get_link($post->ID);
+
+									if($te_link){
+							?>
+									<a href="<?php print $te_link; ?>" class="button">Visit the Site</a>
+							<?php 
+									}
+								} ?>
+
+				<?php endwhile; ?>
+                
+                <?php endif; ?>
+                
+            </div>
+
+<?php get_footer(); ?>
