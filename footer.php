f8126fd56ce351103d81904fc415d0005e7c78f4
Tony Edwards <tony.edwards@gmail.com>
2014-08-21 00:45:07 +0100

    1.2 - Cleanup

--- footer.php
+++ footer.php
@@ -7,12 +7,32 @@
             
             <span id="footer-text">Made with <3</span>
 
+            <?php
+            // get the social links entered via the theme options page
+            $googleplus = get_option('portfolio_googleplus');
+            $twitter = get_option('portfolio_twitter');
+            $linkedin = get_option('portfolio_linkedin');
+            $github = get_option('portfolio_github');
+            $dribbble = get_option('portfolio_dribbble');
+
+            ?>
+
             <ul class="social-links">
-                <li><a href="http://uk.linkedin.com/in/tonyedwardspz"><img src="<?php echo IMAGES ?>/social/linkedin-48x48.png" alt="linkedin" /></a></li>
-                <li><a href="http://www.dribbble.com/tonyedwardspz"><img src="<?php echo IMAGES ?>/social/dribbble-48x48.png" alt="Dribble" /></a></li>
-                <li><a href="http://www.twitter.com/tonyedwardspz"><img src="<?php echo IMAGES ?>/social/twitter-48x48.png" alt="Twitter" /></a></li>
-                <li><a href="http://plus.google.com/109731755368109204853"><img src="<?php echo IMAGES ?>/social/google+-48x48.png" alt="GitHub" /></a></li>
-                <li><a href="http://github.com/tonyedwardspz"><img src="<?php echo IMAGES ?>/social/github-48x48.png" alt="Google+" /></a></li>
+                <?php if($linkedin): ?>
+                <li><a href="<?php print $linkedin; ?>"><img src="<?php echo IMAGES ?>/social/linkedin-48x48.png" alt="linkedin" /></a></li>
+                <?php endif; ?>
+                <?php if($dribbble): ?>
+                <li><a href="<?php print $dribbble; ?>"><img src="<?php echo IMAGES ?>/social/dribbble-48x48.png" alt="Dribble" /></a></li>
+                <?php endif; ?>
+                <?php if($twitter): ?>
+                <li><a href="<?php print $twitter; ?>"><img src="<?php echo IMAGES ?>/social/twitter-48x48.png" alt="Twitter" /></a></li>
+                <?php endif; ?>
+                <?php if($googleplus): ?>
+                <li><a href="<?php print $googleplus; ?>"><img src="<?php echo IMAGES ?>/social/google+-48x48.png" alt="GitHub" /></a></li>
+                <?php endif; ?>
+                <?php if($github): ?>
+                <li><a href="<?php print $github; ?>"><img src="<?php echo IMAGES ?>/social/github-48x48.png" alt="Google+" /></a></li>
+                <?php endif; ?>
             </ul>
             
         </div>
