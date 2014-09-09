4508ea3345981245ee044b2dbaf489207694fbcd
Tony Edwards <tony.edwards@gmail.com>
2014-09-09 14:41:57 +0100

    1.4.3
    ie bug fix. Space between tags showing up in the social links as an
    underscore

--- social-icons.php
+++ social-icons.php
@@ -11,26 +11,22 @@ $dribbble = get_option('portfolio_dribbble');
     <?php if($twitter): ?>
     <li class="twitter">
         <a href="<?php print $twitter; ?>" title="Find me on Twitter" target="_blank">
-            <i class="social_icon fa fa-twitter"></i>
-        </a>
+            <i class="social_icon fa fa-twitter"></i></a>
     </li>
     <?php endif; if($github): ?>
     <li  class="github">
         <a href="<?php print $github; ?>" title="Find me on Github" target="_blank">
-            <i class="social_icon fa fa-github"></i>
-        </a>
+            <i class="social_icon fa fa-github"></i></a>
     </li>
     <?php endif; if($googleplus): ?>
     <li  class="googlePlus">
         <a href="<?php print $googleplus; ?>" title="Find me on Google Plus" target="_blank">
-            <i class="social_icon fa fa-google-plus"></i>
-        </a>
+            <i class="social_icon fa fa-google-plus"></i></a>
     </li>
     <?php endif; if($linkedin): ?>
     <li  class="linkedIn">
         <a href="<?php print $linkedin; ?>" title="Find me on LinkedIn" target="_blank">
-            <i class="social_icon fa fa-linkedin"></i>
-        </a>
+            <i class="social_icon fa fa-linkedin"></i></a>
     </li>
     <?php endif; ?>
 <ul>
