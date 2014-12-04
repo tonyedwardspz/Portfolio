a03c9ab2ab1362def87c5417d0b1926daf0b4bc9
Tony Edwards <tony.edwards@gmail.com>
2014-12-04 16:33:20 +0000

    1.5.10 - Footer social icons
    - Removed original social icons to reduce page size and reduce requests.
    
    - Now using Font Awesome site wide.
    - Floated the icons to left and altered margins / paddings on small
    screens.

--- footer.php
+++ footer.php
@@ -12,33 +12,7 @@
                 </div>
             </span>
 
-            <?php
-            // get the social links entered via the theme options page
-            $googleplus = get_option('portfolio_googleplus');
-            $twitter = get_option('portfolio_twitter');
-            $linkedin = get_option('portfolio_linkedin');
-            $github = get_option('portfolio_github');
-            $dribbble = get_option('portfolio_dribbble');
-
-            ?>
-
-            <ul class="social-links">
-                <?php if($linkedin): ?>
-                <li><a href="<?php print $linkedin; ?>"><img src="<?php echo IMAGES ?>/social/linkedin-48x48.png" alt="linkedin" /></a></li>
-                <?php endif; ?>
-                <?php if($dribbble): ?>
-                <!-- <li><a href="<?php print $dribbble; ?>"><img src="<?php echo IMAGES ?>/social/dribbble-48x48.png" alt="Dribble" /></a></li> -->
-                <?php endif; ?>
-                <?php if($twitter): ?>
-                <li><a href="<?php print $twitter; ?>"><img src="<?php echo IMAGES ?>/social/twitter-48x48.png" alt="Twitter" /></a></li>
-                <?php endif; ?>
-                <?php if($googleplus): ?>
-                <li><a href="<?php print $googleplus; ?>"><img src="<?php echo IMAGES ?>/social/google+-48x48.png" alt="GitHub" /></a></li>
-                <?php endif; ?>
-                <?php if($github): ?>
-                <li><a href="<?php print $github; ?>"><img src="<?php echo IMAGES ?>/social/github-48x48.png" alt="Google+" /></a></li>
-                <?php endif; ?>
-            </ul>
+            <?php include 'social-icons.php'; ?>
             
         </div>
     </footer>
---
+++ whois.php
@@ -0,0 +1,11 @@
+<!-- 
+Whois section for the bottom of a blog post.
+Uses microdata to help with serps.
+ -->
+
+ <div class="micro-about">
+
+ 	<p>Tony Edwards is a semi mature web development student at Plymouth University. I create mobile and
+ 	web applications using time tested technologies. </p>
+
+ </div>
