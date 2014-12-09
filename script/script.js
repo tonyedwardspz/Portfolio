06ac3414eb8bd2133401f81e7e20ad09ec27e3d5
Tony Edwards <tony.edwards@gmail.com>
2014-12-09 00:13:45 +0000

    1.5.15a - conct files
    Added the contact form 7 styles and js into normal styles. Concat using
    prepros.

--- script/script.js
+++ script/script.js
@@ -195,3 +195,7 @@ function changeMenuColor() {
         $j('.menu').css('display', 'block');
     }
 }
+
+// concat the contact form 7 js
+// this will be moved to a conditionally loaded file at some point.
+//@prepros-append wp7contact.js
