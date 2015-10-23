6145123da5592ebaa22f4fe6d06b61fea018aedf
Tony Edwards <tony@plymouthsoftware.com>
2015-10-24 00:19:34 +0100

    Adds formatting for the blog posts

--- blog.php
+++ blog.php
@@ -6,38 +6,38 @@
 
 <?php get_header(); ?>
 
-                <div class="blog-archive BLOGTEMPLATE">   
-                    
-                    <?php if (have_posts() ) : while (have_posts() ) : the_post(); ?>
-        
-                    <div class="unit blog-archive-item two-of-three">
-                        
-                        <h1><span class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></span></h1>
-                        <span class="post-info">By <?php the_author(); ?></span>
-                        
-                        <?php the_content('Read More'); ?>
-                        
-                    </div>
-                    
-                    <div class="unit one-of-three post-meta">
-                        
-                        <?php
-                            if ( has_post_thumbnail() ) {
-                                the_post_thumbnail('blog-post-thumb');
-                            }
-                        ?>
-                        
-                        <span class="post-info"><a href="<?php the_permalink(); ?>"><?php the_time('l F d, Y'); ?></a></span>
-                        <span class="post-info"><?php wp_get_post_tags(); ?></span>
-                    
-                    </div>
-                    
-                    <?php endwhile ; else: ?>
-                        <p><?php _e('No posts were found. Sorry!'); ?></p>
-                    <?php endif; ?>
-                    
-                    <?php get_sidebar(); ?>
-                
-                </div>                
+<div class="blog-archive BLOGTEMPLATE">
+
+    <?php if (have_posts() ) : while (have_posts() ) : the_post(); ?>
+
+    <div class="unit blog-archive-item two-of-three">
+
+        <h1><span class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></span></h1>
+        <span class="post-info">By <?php the_author(); ?></span>
+
+        <?php the_content('Read More'); ?>
+
+    </div>
+
+    <div class="unit one-of-three post-meta">
+
+        <?php
+            if ( has_post_thumbnail() ) {
+                the_post_thumbnail('blog-post-thumb');
+            }
+        ?>
+
+        <span class="post-info"><a href="<?php the_permalink(); ?>"><?php the_time('l F d, Y'); ?></a></span>
+        <span class="post-info"><?php wp_get_post_tags(); ?></span>
+
+    </div>
+
+    <?php endwhile ; else: ?>
+        <p><?php _e('No posts were found. Sorry!'); ?></p>
+    <?php endif; ?>
+
+    <?php get_sidebar(); ?>
+
+</div>
 
 <?php get_footer(); ?>
--- single.php
+++ single.php
@@ -1,42 +1,42 @@
 <?php get_header(); ?>
 
-                <div class="blog-archive SINGLETEMPLATE">
+<div class="blog-archive SINGLETEMPLATE">
 
-                    <?php if (have_posts() ) : while (have_posts() ) : the_post(); ?>
+    <?php if (have_posts() ) : while (have_posts() ) : the_post(); ?>
 
-                    <div class="unit blog-archive-item full-width blog-post">
+    <div class="unit blog-archive-item full-width blog-post">
 
-                        <h1><?php the_title(); ?></h1>
+        <h1><?php the_title(); ?></h1>
+        <span class="post-info"><?php the_time('l F d, Y'); ?></span>
 
-                        <?php
-                            if ( has_post_thumbnail() ) {
-                                the_post_thumbnail('blog-post-thumb');
-                            }
-                        ?>
+        <!-- <span class="post-info"><?php the_tags(); ?></span> -->
 
-                        <span class="post-info"><a href="<?php the_permalink(); ?>"><?php the_time('l F d, Y'); ?></a></span>
-                        <span class="post-info"><?php the_tags(); ?></span>
+    </div>
 
-                    </div>
+    <div class="unit full-width blog-content">
+        <?php
+            if ( has_post_thumbnail() ) {
+                the_post_thumbnail('blog-post-thumb');
+            }
 
-                    <div class="unit full-width">
-                        <?php the_content('Read More'); ?>
-                    </div>
+            the_content();
+        ?>
+    </div>
 
-                    <?php endwhile ; else: ?>
-                        <p><?php _e('No posts were found. Sorry!'); ?></p>
-                    <?php endif; ?>
+    <?php endwhile ; else: ?>
+        <p><?php _e('No posts were found. Sorry!'); ?></p>
+    <?php endif; ?>
 
-                    <div class="micro-about unit">
+    <div class="micro-about unit">
 
-                      <?php echo get_avatar( get_the_author_meta( 'ID' ), $size, $default, $alt, $args ); ?>
+      <?php echo get_avatar( get_the_author_meta( 'ID' ), $size, $default, $alt, $args ); ?>
 
-                        <p>Tony Edwards is a pasty powered developer with <a href="http://plymouthsoftware.com" title="Plymouth Software Development">Plymouth Software</a>, blogger and Web Apps
-                           Student at Plymouth University. When not developing, I can usually be found somewhere spectacular along the Cornish Coast.
-                           You can catch up with me on <a href="http://twitter.com/tonyedwardspz" title="Tony on Twitter" target="_blank">Twitter</a>.</p>
+        <p>Tony Edwards is a pasty powered developer with <a href="http://plymouthsoftware.com" title="Plymouth Software Development">Plymouth Software</a>, blogger and Web Apps
+           Student at Plymouth University. When not developing, I can usually be found somewhere spectacular along the Cornish Coast.
+           You can catch up with me on <a href="http://twitter.com/tonyedwardspz" title="Tony on Twitter" target="_blank">Twitter</a>.</p>
 
-                    </div>
+    </div>
 
-                </div>
+</div>
 
 <?php get_footer(); ?>
