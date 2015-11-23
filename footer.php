fe98f164af57ed646855bf7fc479d78c54a68556
Tony Edwards <tony@plymouthsoftware.com>
2015-11-23 06:50:31 +0000

    Validate schema json

--- footer.php
+++ footer.php
@@ -49,17 +49,19 @@
         "http://uk.linkedin.com/in/tonyedwardspz",
         "https://plus.google.com/109731755368109204853",
         "https://github.com/tonyedwardspz",
-        "https://twitter.com/tonyedwardspz"],
+        "https://twitter.com/tonyedwardspz"
+      ],
       "affiliation": {
         "@type" : "Organization",
-        "name" : "Plymouth University"
+        "name" : "Plymouth University",
         "url" : "https://www.plymouth.ac.uk",
         "contactPoint" : [
           { "@type" : "ContactPoint",
             "telephone" : "+441752600600",
-            "contactType" : "Reception"
-          } ]
-        }
+            "contactType" : "customer service"
+          }
+        ]
+      }
     }
     </script>
 
