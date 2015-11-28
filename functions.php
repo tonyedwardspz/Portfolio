0d82313c21e419f89a02066a8e37ef2a559a0391
Tony Edwards <tony@plymouthsoftware.com>
2015-11-28 10:15:37 +0000

    Remove WordPress emojis

--- functions.php
+++ functions.php
@@ -293,4 +293,8 @@ add_filter( 'image_send_to_editor', 'responsive_editor_filter', 10, 9);
 // prevent brute force amplification attacks againt XMLRPC
 add_filter('xmlrpc_enabled','__return_false');
 
+// Remove WordPress emojis
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+remove_action( 'wp_print_styles', 'print_emoji_styles' );
+
 ?>
